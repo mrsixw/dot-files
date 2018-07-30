10c638a5b7ef25811f5fd3eea422e6d27836abbb
Steve Williams <mrsixw@gmail.com>
2018-07-30 11:06:58 +0100

    Add docker volume remove

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# Clean the containers
 CONTAINERS=`docker ps -aq`
 docker rm $CONTAINERS
+
+# Clean the volumes
+VOLUMES=`docker volume ls -q`
+docker volume rm $VOLUMES
+
 IMAGES=`docker images -q`
 docker rmi $IMAGES
